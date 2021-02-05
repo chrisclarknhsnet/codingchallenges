5faa1d3b085ee3105623c0f79072c437721666ed
Chris Clark <chrisclark@nhs.net>
2021-02-05 10:57:12 +0000

    Adding summat needs checking challenge

---
+++ SummatNeedsChecking/SummatNeedsChecking/SummatNeedsChecking/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SummatNeedsChecking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
