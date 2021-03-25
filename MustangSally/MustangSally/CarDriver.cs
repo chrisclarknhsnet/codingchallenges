c0f0b595ffc1856d650e5e4bb097096ed177eb64
Chris Clark <chrisclark@nhs.net>
2021-03-25 12:07:58 +0000

    Adding Mustang Sally JSON exercise

---
+++ MustangSally/MustangSally/CarDriver.cs
@@ -0,0 +1,12 @@
+﻿namespace Mustang_Sally
+{
+    public class CarDriver
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string car_model { get; set; }
+    }
+}
---
+++ MustangSally/MustangSally/Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Mustang_Sally
+{
+    class Program
+    {
+        private static string cMOCK_DATA_FILE = @"MOCK_DATA.json";
+
+        static void Main(string[] args)
+        {
+            var carDrivers = loadJson();
+            var noofMustangSallys = howManySallyMustangs(carDrivers);
+            Console.WriteLine($"There are {noofMustangSallys} Mustang Sallys");
+            
+            var sallyNonMustangs = areThereAnySallysWhoDontDriveMustangs(carDrivers)
+                ? "are" : "are not";
+
+            Console.WriteLine($"There {sallyNonMustangs} Sallys who do not drive a Mustang");
+            Console.ReadLine();
+        }
+
+        // How many people named Sally drive a Mustang car?
+        private static int howManySallyMustangs(IList<CarDriver> carDrivers)
+        {
+            return carDrivers.Count(
+                cd =>
+                cd.first_name == "Sally" &&
+                cd.car_model == "Mustang");
+        }
+
+        private static bool areThereAnySallysWhoDontDriveMustangs(IList<CarDriver> carDrivers)
+        {
+            return carDrivers.Any(
+                cd =>
+                cd.first_name == "Sally" &&
+                cd.car_model != "Mustang");
+        }
+
+        private static IList<CarDriver> loadJson()
+        {
+            using (var r = new StreamReader(cMOCK_DATA_FILE))
+            {
+                string json = r.ReadToEnd();
+                var items = JsonConvert.DeserializeObject<List<CarDriver>>(json);
+                return items;
+            }
+        }
+    }
+}
